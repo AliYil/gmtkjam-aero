89450c167266af5fad14c27a8ccc938c4565744b
Ali <aliyil332@gmail.com>
2017-07-16 00:31:18 +0300

    Initial commit

---
+++ desktop/src/com/aliyil/aero/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.aliyil.aero.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.aliyil.aero.Aero;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Aero(), config);
+	}
+}
